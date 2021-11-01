ba64c2206c83d187e3a0be52ff5d57560c72fe2d
nanyangbai <bainanyang@outlook.com>
2021-11-01 17:15:27 +0800

    fix: #N/A pass with empty test

--- main_application_demo/src/test/java/com/example/main_application_demo/repository/mapper/PostMapperTest.java
+++ main_application_demo/src/test/java/com/example/main_application_demo/repository/mapper/PostMapperTest.java
@@ -2,18 +2,19 @@ package com.example.main_application_demo.repository.mapper;
 
 import com.example.main_application_demo.IntegrationTest;
 import com.example.main_application_demo.adapter.persistence.PostMapper;
-import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 
 public class PostMapperTest extends IntegrationTest {
     private final PostMapper postMapper;
 
+    @Autowired
     public PostMapperTest(PostMapper postMapper) {
         this.postMapper = postMapper;
     }
 
     @Test
-    void should_save_posts_success(){
-
+    void should_save_posts_success() {
+        //TODO finish test
     }
 }
